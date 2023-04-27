7d152fef4f3231fb0cd062047da076d214faeb96
DenysDovgan <dovgan.denys.ws@gmail.com>
2023-04-27 14:34:06 +0200

    Added Scroll To Top

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { FooterContainer, FooterLink, FooterLinkItems, FooterLinkTitle, FooterLinksContainer, FooterLinksWrapper, FooterWrap, SocialIcons, SocialLogo, SocialMedia, SocialMediaWrap, SocialIconLink, WebsiteRights } from './FooterElements'
 import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa'
+import { animateScroll as scroll } from 'react-scroll'
 
 
 const Footer = () => {
+  const toggleHome = () => {
+    scroll.scrollToTop();
+  };
+
   return (
     <FooterContainer>
       <FooterWrap>
@@ -44,10 +49,10 @@ const Footer = () => {
         </FooterLinksContainer>
         <SocialMedia>
           <SocialMediaWrap>
-            <SocialLogo to='/'>
+            <SocialLogo to='/' onClick={toggleHome}>
               .dataScience
             </SocialLogo>
-            <WebsiteRights>.dataScience © {new Date().getFullYear()} All rights reservved.</WebsiteRights>
+            <WebsiteRights>.dataScience © {new Date().getFullYear()} All rights reserved.</WebsiteRights>
             <SocialIcons>
               <SocialIconLink href="/" target="_blank" aria-label="Facebook">
                 <FaFacebook />
